8ebf6386d958d70026590bf9da54eb9a82a3e498
kelly <ketatiane@gmail.comgit config --global user.email ketatiane@gmail.comgit config --global user.email ketatiane@gmail.com>
2024-06-17 11:25:05 -0300

    primeira commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let usuarios = [{
+    nome: "Rodrigo",
+    email: "Rodrigo@gmail.com",
+    idade: 29,
+}, {
+    nome: "Maria",
+    email: "Maria@gmail.com",
+    idade: 38,
+
+}]
+console.log("Qual email você quer pesquisar?");
+
+process.stdin.on("data", function (data) {
+    let email = data.toString().trim();
+    let usuario;
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].email == email) {
+            usuario = usuarios[i];
+        }
+    }
+
+    if (usuario) {
+        console.log(usuario);
+    } else {
+        console.log("usuario não encontrado");
+    }
+});
+
